b11e4aa7460de167fc2c25926822f2a5579075a9
samiwog-El-roi <samuelogunbodede28@gmail.com>
2025-07-30 19:10:48 +0100

    Order Service included

--- order-service/src/main/java/com/sivalabs/bookstore/orders/domain/models/Customer.java
+++ order-service/src/main/java/com/sivalabs/bookstore/orders/domain/models/Customer.java
@@ -1,11 +1,9 @@
-package com.samiwogelroi.bookstore.webapp.clients.orders;
+package com.sivalabs.bookstore.orders.domain.models;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import java.io.Serializable;
 
 public record Customer(
         @NotBlank(message = "Customer Name is required") String name,
         @NotBlank(message = "Customer email is required") @Email String email,
-        @NotBlank(message = "Customer Phone number is required") String phone)
-        implements Serializable {}
+        @NotBlank(message = "Customer Phone number is required") String phone) {}
--- order-service/src/main/java/com/sivalabs/bookstore/orders/domain/models/OrderItem.java
+++ order-service/src/main/java/com/sivalabs/bookstore/orders/domain/models/OrderItem.java
@@ -1,14 +1,12 @@
-package com.samiwogelroi.bookstore.webapp.clients.orders;
+package com.sivalabs.bookstore.orders.domain.models;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.math.BigDecimal;
 
 public record OrderItem(
         @NotBlank(message = "Code is required") String code,
         @NotBlank(message = "Name is required") String name,
         @NotNull(message = "Price is required") BigDecimal price,
-        @NotNull @Min(1) Integer quantity)
-        implements Serializable {}
+        @NotNull(message = "Quantity is required") @Min(value = 1, message = "Min quantity must be 1") Integer quantity) {}
